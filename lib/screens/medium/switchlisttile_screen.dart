a26fd2329256bc5fb8c714b151ef951323a8b26e
LuisPulenta <luisalbertonu@gmail.com>
2024-05-30 09:24:51 -0300

    SwitchListTile

--- lib/screens/medium/switchlisttile_screen.dart
+++ lib/screens/medium/switchlisttile_screen.dart
@@ -11,36 +11,41 @@ class _SwitchlisttileScreenState extends State<SwitchlisttileScreen> {
   bool _isSwitchOn = false;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Container(
-          height: 300,
-          color: _isSwitchOn ? Colors.orangeAccent : Colors.white,
-          child: Center(
-            child: Text(
-              _isSwitchOn ? 'Light is ON' : 'Light is OFF',
-              style: const TextStyle(
-                fontSize: 18,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SwitchListTile'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            height: 300,
+            color: _isSwitchOn ? Colors.orangeAccent : Colors.white,
+            child: Center(
+              child: Text(
+                _isSwitchOn ? 'Light is ON' : 'Light is OFF',
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        SwitchListTile(
-          title: const Text('Switch List Tile'),
-          value: _isSwitchOn,
-          onChanged: (bool value) {
-            setState(() {
-              _isSwitchOn = value;
-            });
-          },
-          secondary: const Icon(Icons.lightbulb_outline),
-          // subtitle: const Text('Switch List Tile'),
-        )
-      ],
+          const SizedBox(
+            height: 20,
+          ),
+          SwitchListTile(
+            title: const Text('Switch List Tile'),
+            value: _isSwitchOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isSwitchOn = value;
+              });
+            },
+            secondary: const Icon(Icons.lightbulb_outline),
+            // subtitle: const Text('Switch List Tile'),
+          )
+        ],
+      ),
     );
   }
 }
